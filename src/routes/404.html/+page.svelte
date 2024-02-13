67ec119e1dfacbd8c92c936e07a8f6d3f8f744e8
Jack Gutteridge <jack@kingbrick.co.uk>
2024-02-13 15:00:45 +0000

    hi Neil

--- src/routes/404.html/+page.svelte
+++ src/routes/404.html/+page.svelte
@@ -2,6 +2,7 @@
   <h1 class="text-xl mb-4">Page not found</h1>
   <p>The requested page could not be found.</p>
   <a class="underline" href="/">Go back to homepage</a>
+  <img alt="" src="/maths.jpg" class="mt-8 max-w-sm" />
 </div>
 <svelte:head>
   <title>Page not found</title>
