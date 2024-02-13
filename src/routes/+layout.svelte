bc2a351ae78c5903844f9d1ad2c0f61e3a394258
Jack Gutteridge <jack@kingbrick.co.uk>
2024-02-13 14:03:15 +0000

    Navigation rethink

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
   import "../app.css";
+  import { page } from '$app/stores';  
+  $: slug = $page.url.pathname;
 </script>
 <header class="p-4 pb-0">
   <h1 class="text-lg font-bold"><a href="/">Leicester Open Mic Nights</a></h1>
   <p class="text-sm">A list of open mic nights in the Leicestershire area. Showing events in the next 90 days. Select an event for more information.</p>
+  <nav class="mt-8 flex gap-4">
+    <a class={'inline-block font-semibold py-2 px-3 hover:bg-amber-700 rounded-md ' + (slug === '/' ? ' bg-amber-700 outline' : 'bg-amber-900')} href="/">Upcoming events</a>
+    <a class={'inline-block font-semibold py-2 px-3 hover:bg-amber-700 rounded-md ' + (slug === '/featured-events/' ? ' bg-amber-700 outline' : 'bg-amber-900')} href="/featured-events/">Featured open mics</a>
+  </nav>
 </header>
 <slot />
 <footer class="p-4">
--- src/routes/featured-events/+page.svelte
+++ src/routes/featured-events/+page.svelte
@@ -1,24 +1,17 @@
 <script lang="ts">
-  import { getOccurrencesView } from "$lib/utils";
-
-  setTimeout(() => live = true, 0);
-
-  $: live = false;
-  const occurrencesView = getOccurrencesView();
+  import { getEventsView } from "$lib/utils";
+  const eventsView = getEventsView();
 </script>
-<ul class="md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4 p-4 animate-pull grid" class:opacity-0={!live}>
-{#each occurrencesView as {title, hosts, date, slug}}
+<ul class="grid md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4 p-4 animate-pull">
+{#each eventsView as {title, hosts, when, slug}}
   <li class="relative h-40 overflow-hidden rounded-xl grid grid-cols-3 bg-slate-800 hover:outline">
     <div class="p-4 grow col-span-2 text-pretty">
-      <span class="text-amber-400">{ date }</span>
       <h2 class="font-bold">{ title }</h2>
       <span>With { hosts }</span>
+      <span class="text-amber-400 block">{ when }</span>
     </div>
     <img class="w-40 min-w-full h-40 object-cover object-top" src={`/${slug}.jpg`} alt="" />
     <a href={`/events/${slug}`} class="absolute inset-0"><span class="hidden">More information</span></a>
   </li>
 {/each}
 </ul>
-<svelte:head>
-  <title>Leicester Open Mic Nights</title>
-</svelte:head>
