02707f3d2962ec92d96cbb88e62076a6bce57527
Jack Gutteridge <jack@kingbrick.co.uk>
2024-02-14 17:56:15 +0000

    duplicate duplicate

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -2,7 +2,6 @@
   <h1 class="text-xl mb-4 font-semibold">About the Leicester Open Mics app</h1>
   <p class="mb-4">The Leicester Open Mics app is a project by <a class="underline" target="_blank" href="https://www.jng.me.uk/">Jack Gutteridge</a> and Neil Williamson. We regularly attend open mics in the Leicestershire area.</p>
   <p class="mb-4">We built this app so we wouldn't have to manually keep track of when the next event was on. As comical as it is, we want to put a stop to hosts scrambling around trying to figure out when their next event is at the end of every night!</p>
-  <p class="mb-4">We built this app so we wouldn't have to manually keep track of when the next event was on. As comical as it is, we want to put a stop to hosts scrambling around trying to figure out when their next event is at the end of every night!</p>
   <p class="mb-4"><a class="underline" href="/">Go back to homepage</a></p>
 </div>
 <svelte:head>
